4b48c67eacc5e71aa7e557e366e0d84df73cd667
Vikas Pandey <vikaspandey00838@gmail.com>
2025-08-21 11:58:53 +0530

    just creating some custom hooks for displaying movies list.

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -4,9 +4,13 @@ import useNowPlayingMovies from '../hooks/useNowPlayingMovies'
 import SecondaryContainer from './SecondaryContainer';
 import MainContainer from './mainContainer';
 import { useSelector } from 'react-redux';
+import usePopularMovies from '../hooks/usePopularMovies';
+import useTopRatedMovies from '../hooks/useTopRatedMovies';
 
 const Browse = () => {
   useNowPlayingMovies();
+  usePopularMovies();
+  useTopRatedMovies();
 
   return (
     <div>
--- src/components/SecondaryContainer.jsx
+++ src/components/SecondaryContainer.jsx
@@ -6,15 +6,16 @@ const SecondaryContainer = () => {
   const movies = useSelector((store) => store.movies);
   console.log(movies);
   return (
-    <div className='bg-black'>
+   movies && (
+     <div className='bg-black'>
       <div className='-mt-60 pl-12 relative z-20'>
         <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
-        <MovieList title={"Trending"} movies={movies.nowPlayingMovies} />
-        <MovieList title={"Action Movies"} movies={movies.nowPlayingMovies} />
-        <MovieList title={"Horror"} movies={movies.nowPlayingMovies} />
+        <MovieList title={"Popular"} movies={movies.popularMovies} />
+        <MovieList title={"Top Rated"} movies={movies.topRatedMovies} />
         <MovieList title={"Upcoming"} movies={movies.nowPlayingMovies} />
       </div>
     </div>
+   )
   )
 }
 
